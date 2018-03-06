58f3433f73930cc93e52da6d616203236876a7b1
Andrew Hammett <hammett@amh.io>
2018-03-05 17:52:06 -0800

    simple web app that will return something on root and provide a health check

---
+++ web-app/src/web.py
@@ -0,0 +1,23 @@
+import os
+import SimpleHTTPServer
+import SocketServer
+
+WEB_PORT = os.getenv('WEB_PORT', 8000)
+WEB_PATH = os.path.join(os.getcwd(), 'static')
+
+
+def run_web():
+    Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+    httpd = SocketServer.TCPServer(("", WEB_PORT), Handler)
+
+    print "Serving at port {} in path {}".format(WEB_PORT, WEB_PATH)
+    os.chdir(WEB_PATH)
+    httpd.serve_forever()
+
+
+def main():
+    run_web()
+
+
+if __name__ == '__main__':
+    main()
